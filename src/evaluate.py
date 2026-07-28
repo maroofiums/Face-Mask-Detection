e7caf232e09343dc7d9b351ea6e2558b8a4398df
maroofiums <maroof96965@gmail.com>
2026-07-28 18:56:36 +0500

    Train and Evaluate Model

---
+++ src/evaluate.py
@@ -0,0 +1,62 @@
+import torch
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    classification_report,
+    confusion_matrix
+)
+
+from config import DEVICE, MODEL_PATH
+from model import create_model
+from dataset import get_dataloaders
+
+def evaluate():
+
+    _, val_loader = get_dataloaders()
+
+    model = create_model().to(DEVICE)
+    model.load_state_dict(
+        torch.load(
+            MODEL_PATH,
+            map_location=DEVICE
+        )
+    )
+
+    all_labels = []
+    all_predictions = []
+
+    with torch.no_grad():
+        for images, labels in val_loader:
+
+            images = images.to(DEVICE)
+            labels = labels.to(DEVICE)
+
+            outputs = model(images)
+
+            _, predictions = torch.max(outputs, 1)
+
+            all_labels.extend(labels.numpy())
+            all_predictions.extend(predictions.cpu().numpy())
+
+    print("=" * 50)
+
+    print(f"Accuracy : {accuracy_score(all_labels, all_predictions):.4f}")
+    print(f"Precision: {precision_score(all_labels, all_predictions):.4f}")
+    print(f"Recall   : {recall_score(all_labels, all_predictions):.4f}")
+    print(f"F1 Score : {f1_score(all_labels, all_predictions):.4f}")
+
+    print("\nConfusion Matrix")
+    print(confusion_matrix(all_labels, all_predictions))
+
+    print("\nClassification Report")
+    print(classification_report(
+        all_labels,
+        all_predictions,
+        target_names=["Mask", "No Mask"]
+    ))
+
+
+if __name__ == "__main__":
+    evaluate()
---
+++ src/train.py
@@ -0,0 +1,104 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from tqdm import tqdm
+
+from config import DEVICE, EPOCHS, LEARNING_RATE, MODEL_PATH
+from dataset import get_dataloaders
+from model import create_model
+
+
+def train():
+    train_loader, val_loader = get_dataloaders()
+
+    model = create_model().to(DEVICE)
+
+    criterion = nn.CrossEntropyLoss()
+
+    optimizer = optim.Adam(
+        model.classifier.parameters(),
+        lr=LEARNING_RATE
+    )
+
+    best_accuracy = 0.0
+
+    for epoch in range(EPOCHS):
+
+        model.train()
+
+        train_loss = 0
+        train_correct = 0
+        train_total = 0
+
+        progress_bar = tqdm(train_loader)
+
+        for images, labels in progress_bar:
+
+            images = images.to(DEVICE)
+            labels = labels.to(DEVICE)
+
+            optimizer.zero_grad()
+
+            outputs = model(images)
+
+            loss = criterion(outputs, labels)
+
+            loss.backward()
+
+            optimizer.step()
+
+            train_loss += loss.item()
+
+            _, predicted = torch.max(outputs, 1)
+
+            train_total += labels.size(0)
+
+            train_correct += (predicted == labels).sum().item()
+
+            progress_bar.set_description(
+                f"Epoch {epoch+1}/{EPOCHS}"
+            )
+
+        train_accuracy = 100 * train_correct / train_total
+
+        model.eval()
+
+        val_correct = 0
+        val_total = 0
+
+        with torch.no_grad():
+            for images, labels in val_loader:
+                images = images.to(DEVICE)
+                labels = labels.to(DEVICE)
+
+                outputs = model(images)
+
+                _, predicted = torch.max(outputs, 1)
+
+                val_total += labels.size(0)
+                val_correct += (predicted == labels).sum().item()
+
+        val_accuracy = 100 * val_correct / val_total
+
+        print(
+            f"Epoch [{epoch+1}/{EPOCHS}]"
+            f"Train Accuracy: {train_accuracy:.2f}%"
+            f"Validation Accuracy: {val_accuracy:.2f}%"
+        )
+
+        if val_accuracy > best_accuracy:
+            best_accuracy = val_accuracy
+
+            torch.save(
+                model.state_dict(),
+                MODEL_PATH
+            )
+
+            print("Best Model Saved!")
+
+    print("Training Completed!...")
+        
+
+if __name__ == "__main__":
+    train()
